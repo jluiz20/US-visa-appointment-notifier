4c9b74b18102b74c956d2eba7b4c845bebaa2032
João Luiz Vieira <vieira.jluiz@gmail.com>
2023-03-28 07:21:01 -0700

    fix wake up time

--- config.js
+++ config.js
@@ -21,8 +21,8 @@ module.exports = {
   NEXT_SCHEDULE_POLL_MIN: process.env.NEXT_SCHEDULE_POLL_MIN || 15, // default to 15 minutes
   MAX_NUMBER_OF_POLL: process.env.MAX_NUMBER_OF_POLL || 250, // number of polls before stopping
   NOTIFY_ON_DATE_BEFORE: process.env.NOTIFY_ON_DATE_BEFORE, // in ISO format i.e YYYY-MM-DD
-  SLEEP_HOUR: process.env.SLEEP_HOUR || 22, // in 24 hour format, i.e 23
-  WAKEUP_HOUR: process.env.WAKEUP_HOUR || 07, // in 24 hour format, i.e 07
+  SLEEP_HOUR: process.env.SLEEP_HOUR || 22, // in 24-hour format, i.e 23
+  WAKEUP_HOUR: process.env.WAKEUP_HOUR || 7, // in 24-hour format, i.e 07
 
   NOTIFY_EMAILS: process.env.NOTIFY_EMAILS, // comma separated list of emails
   mailgun: {
